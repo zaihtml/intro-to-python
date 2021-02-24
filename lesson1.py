5cc676c17f0b4d50dfb448d82ce5f677e6836bb4
zaihtml <67802675+zaihtml@users.noreply.github.com>
2021-02-24 21:50:41 +0000

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+number_of_cats = 10
+cans_per_cat = 2
+
+output = "{} cans are each day needed to feed all the cats.".format(number_of_cats * cans_per_cat)
+
+print(output)
+
+output_week = "{} cans are needed each week to feed all the cats.".format((number_of_cats * cans_per_cat) * 7)
+
+print(output_week)
+
+#comment
+"""This is a comment
+that goes across multiple
+lines."""
