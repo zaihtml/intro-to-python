b1af3800b6fca944805be86b99847c8db82cc7da
zaihtml <67802675+zaihtml@users.noreply.github.com>
2021-02-25 21:56:40 +0000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,96 @@
+"""This program prints the letter 'o' for each number between 1-100."""
+
+"""for number in range(100):
+    output = 'o'*number
+    print(output)"""
+
+"""def calculate_vat(amount):
+    total = amount * 1.2
+    print(total)
+
+calculate_vat(100)"""
+
+import turtle
+
+"""turtle.color('red', 'red')
+turtle.begin_fill()
+turtle.speed(2)
+
+#start triangle
+turtle.forward(200)
+turtle.left(120)
+
+turtle.forward(200)
+turtle.left(120)
+
+turtle.forward(200)
+turtle.left(30)
+#end triangle
+#start square
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+#end square
+turtle.forward(50)
+turtle.left(90)
+
+turtle.forward(50)
+turtle.left(90)
+#begin window
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(125)
+# start 2nd window
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+# 2nd window end
+
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+#begin door
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(50)
+# door end
+turtle.left(90)
+turtle.forward(90)
+
+turtle.end_fill()
+turtle.done()"""
+
+
